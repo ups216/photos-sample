4252d21876d962347ab760ade64e5631beb96ab0
Lei Xu <leixu@leansoftx.com>
2023-03-17 10:26:30 +0800

    Create Sample Project base structure

---
+++ src/main/java/com/example/photos/Photo.java
@@ -0,0 +1,52 @@
+package com.example.photos;
+
+public class Photo {
+
+    private String id;
+
+    private String fileName;
+
+    private String contentType;
+
+    private byte[] fileContent;
+
+    public Photo() {
+    }
+
+    public Photo(String id, String fileName) {
+        this.id = id;
+        this.fileName = fileName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public byte[] getFileContent() {
+        return fileContent;
+    }
+
+    public void setFileContent(byte[] fileContent) {
+        this.fileContent = fileContent;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+}
